3fe2e615fa7ad12a6b42e90e2bbee73f7cd3e329
dcvan <dcvan@cs.unc.edu>
2018-11-13 15:45:29 -0500

    [cephfs-api] ensured atomicity of volume deletion

--- cephfs-api/container.py
+++ cephfs-api/container.py
@@ -1,5 +1,6 @@
 import shlex
 
+from tornado.gen import sleep
 from util import Singleton, Loggable, AsyncHttpClientWrapper
 
 
@@ -11,7 +12,7 @@ class MarathonClient(Loggable, metaclass=Singleton):
     self.__cli = AsyncHttpClientWrapper()
 
   async def create_container(self, name, group, image, command, privileged=False, network='host',
-                             cpus=1, mem=2048, env={}, volumes={}):
+                             cpus=.1, mem=256, env={}, volumes={}):
     req = dict(id='/%s/%s'%(group, name),
                cpus=cpus, mem=mem, disk=0,
                args=shlex.split(command),
@@ -26,6 +27,16 @@ class MarathonClient(Loggable, metaclass=Singleton):
     return await self.__cli.post(self.__masters[0], self.__port, '/v2/apps', req)
 
   async def delete_container(self, name, group):
-    return await self.__cli.delete(self.__masters[0], self.__port, '/v2/apps/%s/%s'%(group, name))
+    master, port = self.__masters[0], self.__port
+    status, msg, err = await self.__cli.delete(master, port, '/v2/apps/%s/%s'%(group, name))
+    if status != 200:
+      return status, msg, err
+    _, deployments, _ = await self.__cli.get(master, port, '/v2/deployments')
+    self.logger.info(deployments)
+    self.logger.info(any(['/%s/%s'%(group, name) in d.get('affectedApps', []) for d in deployments]))
+    while any(['/%s/%s'%(group, name) in d.get('affectedApps', []) for d in deployments]):
+      _, deployments, _ = await self.__cli.get(master, port, '/v2/deployments')
+      await sleep(1)
+    return status, msg, err
 
 
--- cephfs-api/server.py
+++ cephfs-api/server.py
@@ -1,7 +1,7 @@
 import sys
 import tornado
-import asyncio
 
+from tornado.gen import sleep
 from tornado.web import RequestHandler
 
 from tornado.httpserver import HTTPServer
@@ -46,45 +46,39 @@ class FileSystemsHandler(RequestHandler):
   async def post(self):
     """
     {
-      "name": "<fs-name>",
-      "appliance": "<appliance-name>"
+      "name": "<fs-name>"
     }
-    :return:
+
     """
     marathon = self.__marathon
     ceph_daemon_image = options.ceph_daemon_image
     cfg_dir, lib_dir = options.ceph_config_dir, options.ceph_lib_dir
     req = json_decode(self.request.body)
-    name, app = req.get('name'), req.get('appliance')
-    if not name or not app:
+    name = req.get('name')
+    if not name:
       self.set_status(400)
-      if not name:
-        err_msg = '`name` is required'
-      elif not app:
-        err_msg = '`appliance` is required'
-      self.write(error(400, err_msg))
-    fs_name = '%s-%s'%(app, name)
+      self.write(error(400, '`name` is required'))
 
     def volume(container_path, host_path, mode):
       return dict(containerPath=container_path, hostPath=host_path, mode=mode)
 
-    status, c, err = await marathon.create_container('%s-%s'%(app, name), 'cephfs', ceph_daemon_image,
+    status, c, err = await marathon.create_container(name, 'cephfs', ceph_daemon_image,
                                                      'mds',env=dict(CEPHFS_CREATE=1,
-                                                                    CEPHFS_NAME=fs_name,
-                                                                    MDS_NAME='mds-%s'%fs_name),
+                                                                    CEPHFS_NAME=name,
+                                                                    MDS_NAME='mds-%s'%name),
                                                      volumes=[
                                                        volume('/etc/ceph', cfg_dir, 'RW'),
                                                        volume('/var/lib/ceph', lib_dir, 'RW')
                                                      ])
     self.set_status(status)
     if status == 200:
-      get_fs_status, _, _ = await self.__ceph.get_fs(fs_name)
+      get_fs_status, _, _ = await self.__ceph.get_fs(name)
       while get_fs_status != 200:
-        get_fs_status, _, _ = await self.__ceph.get_fs(fs_name)
-        await asyncio.sleep(1)
+        get_fs_status, _, _ = await self.__ceph.get_fs(name)
+        await sleep(1)
     elif status == 409:
-      err = error(status, 'Filesystem `%s` for `%s` already exists' % (name, app))
-    self.write(message(status, 'Filesystem `%s` for `%s` is created'%(name, app)) if c else err)
+      err = error(status, 'Filesystem `%s` already exists'%name)
+    self.write(message(status, 'Filesystem `%s` is created'%name) if c else err)
 
 
 class FileSystemHandler(RequestHandler):
@@ -93,23 +87,25 @@ class FileSystemHandler(RequestHandler):
     self.__marathon = MarathonClient(options.marathon_masters, options.marathon_port)
     self.__ceph = Ceph(options.ceph_api_host, options.ceph_api_port)
 
-  async def get(self, app, name):
-    status, msg, err = await self.__ceph.get_fs('%s-%s'%(app, name))
+  async def get(self, name):
+    status, msg, err = await self.__ceph.get_fs(name)
     self.set_status(status)
     self.write(msg if status == 200 else err)
 
-  async def delete(self, app, name):
-    status, msg, err = await self.__marathon.delete_container('%s-%s'%(app, name), 'cephfs')
+  async def delete(self, name):
+    status, msg, err = await self.__marathon.delete_container(name, 'cephfs')
     if status == 404:
-      err = error(status, 'Filesystem `%s` for appliance `%s` is not found'%(name, app))
+      err = error(status, 'Filesystem `%s` is not found'%name)
     if status != 200:
       self.set_status(status)
       self.write(err)
       return
-    status, msg, err = await self.__ceph.clean_fs('%s-%s'%(app, name))
+    status, msg, err = await self.__ceph.clean_fs(name)
+    while status != 200:
+      status, msg, err = await self.__ceph.clean_fs(name)
+      await sleep(1)
     self.set_status(status)
-    self.write(message(status, 'Filesystem `%s` for appliance `%s` has been deleted'%(app, name))
-               if status == 200 else err)
+    self.write(message(status, 'Filesystem `%s` has been deleted' % name) if status == 200 else err)
 
 
 if '__main__' == __name__:
@@ -117,7 +113,7 @@ if '__main__' == __name__:
   validate_marathon_masters()
   app = tornado.web.Application([
     (r'/fs', FileSystemsHandler),
-    (r'/appliance/([a-zA-Z0-9-]+)/fs/([a-zA-Z0-9-]+)\/*', FileSystemHandler),
+    (r'/fs/([a-zA-Z0-9-]+)\/*', FileSystemHandler),
   ])
   server = HTTPServer(app)
   server.listen(options.port, address=options.address)
